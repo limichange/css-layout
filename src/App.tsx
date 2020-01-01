d6e9ddeef41a09b3da832b2b8dea19ae6bf22222
limichange <limichange@hotmail.com>
2020-01-01 17:03:49 +0800

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import Layout1 from './Layout1'
+import Layout3 from './Layout3'
+import Layout2 from './Layout2'
 
 function App() {
   return (
     <div className='App'>
       <Layout1></Layout1>
+      <Layout2></Layout2>
+      <Layout3></Layout3>
     </div>
   )
 }

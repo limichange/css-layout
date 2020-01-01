b1ac6749342f8b47100e27ec04fa4d16f2344659
limichange <limichange@hotmail.com>
2020-01-01 20:46:02 +0800

    Update

--- src/Layout1/index.tsx
+++ src/Layout1/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import './index.css'
 
 export default function Layout1() {
-  return <div className='Layout1'>Layout1</div>
+  return (
+    <div className='Layout1'>
+      <div className='Header'></div>
+      <div className='Content'></div>
+      <div className='Footer'></div>
+    </div>
+  )
 }

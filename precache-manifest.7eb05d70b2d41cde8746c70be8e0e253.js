2fc1650e17fd6ef010c0fa5d17f9ff8c4d43afa6
limichange <limichange@users.noreply.github.com>
2020-03-16 13:23:34 +0000

    Automated deployment: Mon Mar 16 13:23:34 UTC 2020 113397bb1820964c67653814ae01db5bea94736b

---
+++ precache-manifest.7eb05d70b2d41cde8746c70be8e0e253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60cc5b968c058a5ee9aae00d0b3cb93d",
+    "url": "/css-layout/index.html"
+  },
+  {
+    "revision": "8ad0cc9964d1dfe0b314",
+    "url": "/css-layout/static/css/main.9245c298.chunk.css"
+  },
+  {
+    "revision": "ce3a610ea4ae5ea3e85e",
+    "url": "/css-layout/static/js/2.41f8d1c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/css-layout/static/js/2.41f8d1c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ad0cc9964d1dfe0b314",
+    "url": "/css-layout/static/js/main.f8da9018.chunk.js"
+  },
+  {
+    "revision": "d696b539ed6ba165d534",
+    "url": "/css-layout/static/js/runtime-main.2ac98e13.js"
+  }
+]);
